14065ee2b07d479f0e8c3d2b32fe9f53dd5e923b
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2019-05-10 01:48:54 +0200

    [FIX] Any errors with last integration
     - layer searching list
     - breadcums with href
     - clean carousel div
     - change hide event search list
     - correct order breadcums

--- code/starwars/starwarsapp/views.py
+++ code/starwars/starwarsapp/views.py
@@ -36,6 +36,7 @@ def show_film(request, id):
 
 
 def bread_cums(request):
+    # TODO: Review inverses... fixing with debuging... (too many??)
     if request.method == 'GET':
         if LAST_URIS not in request.session or not request.session[LAST_URIS]:
             last_uris_list = [request.path]
@@ -44,11 +45,13 @@ def bread_cums(request):
             last_uris_list = request.session[LAST_URIS]
             last_uris_list.append(request.path)
 
-            # Filter duplicates
-            last_uris_list = list(set(last_uris_list))
+            # Filter duplicates persist order
+            seen = set()
+            seen_add = seen.add
+            last_uris_list = [uri for uri in last_uris_list[::-1] if not (uri in seen or seen_add(uri))][::-1]
 
             # Last n uris
-            last_uris_list = last_uris_list[:-(MAX_BREAD_CUMS+1):-1]
+            last_uris_list = last_uris_list[:-(MAX_BREAD_CUMS+1):-1][::-1]
 
             request.session[LAST_URIS] = last_uris_list
 
